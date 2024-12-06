c300ddef0b895b62663707146ea1227a50def3a3
Manish Mittal <162432570+manishmittal08@users.noreply.github.com>
2024-12-06 23:39:11 +0530

    Update SQL_PRACTICE.sql

--- SQL_PRACTICE.sql
+++ SQL_PRACTICE.sql
@@ -1,5 +1,14 @@
+--Include below given SELECT queries before you prepare your SELECT queries:
+UPDATE Admissions
+SET attending_doctor_id = 29
+WHERE attending_doctor_id = 3;
+UPDATE Admissions
+SET patient_id = 4
+WHERE patient_id = 35;
+SELECT COUNT(*) FROM admissions WHERE attending_doctor_id = 3;
+
 1. QUERY ANSWER:-
-SELECT d.*
+SELECT DISTINCT d.*
 FROM Doctors AS d
 JOIN Admissions AS a ON a.attending_doctor_id = d.doctor_id;
 
@@ -12,8 +21,10 @@ WHERE a.attending_doctor_id IS NULL;
 
 
 3. QUERY ANSWER:-
-SELECT p.*
-FROM Patients AS p
-JOIN Admissions AS a ON a.patient_id = p.patient_id
-WHERE a.attending_doctor_id IS NULL;
+SELECT DISTINCT p.*
+FROM patients AS p
+JOIN admissions AS a ON p.patient_id = a.patient_id
+LEFT JOIN doctors d ON a.attending_doctor_id = d.doctor_id
+WHERE a.attending_doctor_id IS NULL OR d.doctor_id IS NULL;
+
 
